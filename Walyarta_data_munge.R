0e8ba6122a44a630edc51691cb0ad0d9c4be1e10
Bartesto <barthuntley@iinet.net.au>
2015-01-13 16:56:40 +0800

    Got dplyr multiple group by summary working and changed objects to lc

--- Walyarta_data_munge.R
+++ Walyarta_data_munge.R
@@ -1,8 +1,22 @@
 rm(list = ls(all = T))
 
+library(dplyr)
+
 setwd("Z:\\DEC\\Eighty_Mile_Beach_and_Walyarta_Conservation_Program\\DATA\\analysis\\20140916\\raw_data")
 
-FD <- read.csv("Walyarta_All_Field_Data_20140916_forAnalysis.csv", header = TRUE)
-FD$FieldDate <- as.Date(FD$FieldDate, "%d/%m/%Y")
+fd <- read.csv("Walyarta_All_Field_Data_20140916_forAnalysis.csv", header = TRUE)
+fd$FieldDate <- as.Date(fd$FieldDate, "%d/%m/%Y")
 
-IND <- read.csv("Indices_l8ut11174m_160914_USG_utm51pre_stackR.csv", header = TRUE)
+ind <- read.csv("Indices_l8ut11174m_160914_USG_utm51pre_stackR.csv", header = TRUE)
+
+glimpse(fd)
+summarise(fd,avg=mean(FCNadirSelected))
+
+a <- fd %>%
+        group_by(SiteID) %>%
+        summarise(
+                avgFC=mean(FCNadirSelected),
+                avgZN=mean(ZenithSelected),
+                avgAE=mean(AerialCanopyCover))
+
+
