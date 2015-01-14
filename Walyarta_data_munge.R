93cc548e16f384c38260e669ef6c8453534796cd
Bartesto <barthuntley@iinet.net.au>
2015-01-14 11:30:38 +0800

    Got dplyr piping summary working for Muir factors

--- Walyarta_data_munge.R
+++ Walyarta_data_munge.R
@@ -2,21 +2,28 @@ rm(list = ls(all = T))
 
 library(dplyr)
 
-setwd("Z:\\DEC\\Eighty_Mile_Beach_and_Walyarta_Conservation_Program\\DATA\\analysis\\20140916\\raw_data")
+setwd(paste0("Z:\\DEC\\Eighty_Mile_Beach_and_Walyarta_Conservation_",
+      "Program\\DATA\\analysis\\20140916\\raw_data"))
 
-fd <- read.csv("Walyarta_All_Field_Data_20140916_forAnalysis.csv", header = TRUE)
+fd <- read.csv("Walyarta_All_Field_Data_20140916_forAnalysis.csv",
+               header = TRUE)
 fd$FieldDate <- as.Date(fd$FieldDate, "%d/%m/%Y")
 
-ind <- read.csv("Indices_l8ut11174m_160914_USG_utm51pre_stackR.csv", header = TRUE)
+ind <- read.csv("Indices_l8ut11174m_160914_USG_utm51pre_stackR.csv",
+                header = TRUE)
 
 glimpse(fd)
-summarise(fd,avg=mean(FCNadirSelected))
+)
 
-a <- fd %>%
+
+#works
+fd %>%
         group_by(SiteID) %>%
         summarise(
                 avgFC=mean(FCNadirSelected),
                 avgZN=mean(ZenithSelected),
-                avgAE=mean(AerialCanopyCover))
+                avgAE=mean(AerialCanopyCover),
+                avgMR=mean(MuirVegetationClass))
+
 
 
