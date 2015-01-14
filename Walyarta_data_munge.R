c86f8542a4d35dd2f32a04b0ef141fca12e30d16
Bartesto <barthuntley@iinet.net.au>
2015-01-14 13:21:16 +0800

    clean up whitespace

--- Walyarta_data_munge.R
+++ Walyarta_data_munge.R
@@ -1,6 +1,8 @@
 rm(list = ls(all = T))
 
 library(dplyr)
+library(lattice)
+library(car)
 
 setwd(paste0("Z:\\DEC\\Eighty_Mile_Beach_and_Walyarta_Conservation_",
       "Program\\DATA\\analysis\\20140916\\raw_data"))
@@ -14,7 +16,7 @@ ind <- read.csv("Indices_l8ut11174m_160914_USG_utm51pre_stackR.csv",
 
 glimpse(fd)
 
-#works
+#Field data munging
 fd_out <- fd %>%
                 group_by(SiteID) %>%
                 summarise(
@@ -23,9 +25,26 @@ fd_out <- fd %>%
                         avgAE=mean(AerialCanopyCover),
                         avgMR=mean(MuirVegetationClass),
                         avgTP=mean(Template, na.rm=TRUE))
+#labels for MUIR
+MRlab <- c("dtm", "lmw", "tm", "otm", "dlg", "tm", "tm", "otm", "tm", "votm",
+  "dsc", "mdhg", "dhg", "otm", "oh", "lg", "tm", "volg", "h", "tm",
+  "dsd", "otm", "dlg", "votm", "s", "mdhg", "otm", "dtm", "volg", "lhc")
+fd_out <- cbind(fd_out, MRlab)
 
+
+#Index from image munging
 ind_out <- ind %>%
                   mutate(b234=(-4 * b2) - (b3) + (2 * b4))
-        
+
+#Combining datasets
 all_data <- inner_join(fd_out, ind_out, by="SiteID")
+
+
+#Analysis graphs
+pairs(~avgFC+avgZN+avgAE+avgTP+i35+ndvi+b234, data=all_data,
+      main="Pairs Scatterplot for Walyarta Data")
+
+scatterplot.matrix(~avgFC+avgZN+avgAE+avgTP+i35+ndvi+b234|MRlab,
+                   data=all_data)
+
 
